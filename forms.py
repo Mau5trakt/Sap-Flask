ed28a1bfdc009106e4148b293ad976940f6d49d6
Mau5trakt <kubzalemany@hotmail.com>
2022-05-05 00:03:55 -0600

    Created persona_form Class

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class PersonaForm(FlaskForm):
+    nombre = StringField('Nombre', validators=[DataRequired()])
+    apellido = StringField('Apellido')
+    email = StringField('email', validators=[DataRequired()])
+    enviar = SubmitField('Enviar')
+
